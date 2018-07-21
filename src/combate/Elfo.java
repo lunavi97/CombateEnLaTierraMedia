48edd6b078d97c9601ff65d838fd40da71eac3ab
Luciano Nahuel Vitale <lunavi97@gmail.com>
2018-07-21 02:58:30 -0300

    Desarrollado Elfo

--- src/combate/Elfo.java
+++ src/combate/Elfo.java
@@ -2,19 +2,31 @@ package combate;
 
 public class Elfo extends Personaje {
 
+    private final int FUERZAINICIAL = 5;
+
+    public Elfo() {
+        fuerza = FUERZAINICIAL;
+    }
+
     @Override
     public void vivo_atacar(Personaje otro) {
-        
+        otro.sufrirAtaque(fuerza);
     }
 
     @Override
     public void vivo_descansar() {
-        
+        vida = VIDAINICIAL;
+        fuerza = FUERZAINICIAL;
     }
 
     @Override
     public void vivo_sufrirAtaque(int ataqueRecibido) {
-        
+        if (vida > ataqueRecibido) {
+            vida -= ataqueRecibido;
+            fuerza += FUERZAINICIAL;
+        }
+        else
+            vida = 0;
     }
 
 
