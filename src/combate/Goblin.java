a095d8432d0a5dedb485d8a67ba9deb7d1d6750b
Luciano Nahuel Vitale <lunavi97@gmail.com>
2018-07-22 02:57:51 -0300

    Desarrollado Goblin

--- src/combate/Goblin.java
+++ src/combate/Goblin.java
@@ -1,20 +1,41 @@
 package combate;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 public class Goblin extends Personaje {
 
-    @Override
-    public void vivo_atacar(Personaje otro) {
-        
-    }
+  private final int FUERZAINICIAL = 3;
+  private final int probabilidadDeSerGolpeado = 1/10;
 
-    @Override
-    public void vivo_descansar() {
-        
-    }
+  public Goblin() {
+    fuerza = FUERZAINICIAL;
+  }
 
-    @Override
-    public void vivo_sufrirAtaque(int ataqueRecibido) {
-        
-    }
+  @Override
+  public void vivo_atacar(Personaje otro) {
+    otro.sufrirAtaque(fuerza);
+  }
+
+  @Override
+  public void vivo_descansar() {
+    vida = VIDAINICIAL;
+  }
+
+  @Override
+  public void vivo_sufrirAtaque(int ataqueRecibido) {
+    if (!esGolpeado()) return;
+
+    final int golpe = ataqueRecibido;
+
+    if (vida > golpe)
+      vida -= golpe;
+
+    else vida = 0;
+  }
+
+  private boolean esGolpeado() {
+    ThreadLocalRandom random = ThreadLocalRandom.current();
+    return random.nextInt(probabilidadDeSerGolpeado) == 0;
+  }
 
 }
